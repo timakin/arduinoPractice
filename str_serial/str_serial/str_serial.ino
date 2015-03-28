b2c410fae418c45b710e003e0b46ab33a5979c09
timakin <timaki.st@gmail.com>
2015-03-28 11:02:06 +0900

    serial multiple string

--- str_serial/str_serial/str_serial.ino
+++ str_serial/str_serial/str_serial.ino
@@ -1,14 +1,25 @@
+char input[30];   // 文字列格納用
+int i = 0;  // 文字数のカウンタ
+      
 void setup() {
   Serial.begin(9600);
-  // ツール>シリアルモニタで試せる
 }
-
+      
 void loop() {
-  // put your main code here, to run repeatedly:
-  int input;
-  input = Serial.read();
-  if(input != -1){
-    Serial.write(input);
-    Serial.write("\n");
+   
+  // データ受信したとき
+  if (Serial.available()) {
+    input[i] = Serial.read();
+     // 文字数が30以上 or 末尾文字
+    if (i > 30 || input[i] == '.') {
+      // 末尾に終端文字の挿入
+      input[i] = '\0';
+      // 受信文字列を送信
+      Serial.write(input);
+      Serial.write("\n");
+      // カウンタの初期化
+      i = 0;
+    }
+    else { i++; }
   }
 }
